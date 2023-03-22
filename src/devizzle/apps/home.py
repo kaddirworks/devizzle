b92430a510bf817a4899de4e359f2205e807e829
Wesley Barros <wesleybarros950@gmail.com>
2023-03-22 08:55:15 -0300

    removed home app

--- src/devizzle/apps/home.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="", tags=["home"])
-
-
-@router.get("/")
-def home():
-    return {"message": "Hello, world!"}
--- src/devizzle/main.py
+++ src/devizzle/main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from devizzle.core.auth import auth
-from devizzle.apps import home
 from devizzle.apps.bottles import bottles
 from devizzle.settings import settings
 
 devizzle = FastAPI(title="Devizzle")
 devizzle.include_router(auth.router)
 devizzle.include_router(bottles.router)
-devizzle.include_router(home.router)
 
 origins = [settings.allowed_origin]
 
