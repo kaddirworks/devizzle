c004679790a5a3a343bc1134a88b9f93a9ea9a51
Wesley Barros <wesleybarros950@gmail.com>
2023-03-25 08:48:03 -0300

    preparation for mysql db

--- devizzle/apps/auth/models.py
+++ devizzle/apps/auth/models.py
@@ -8,13 +8,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, nullable=False)
+    username = Column(String(128), unique=True, index=True)
+    email = Column(String(128), unique=True, nullable=False)
 
     reg_date = Column(DateTime, default=datetime.now)
     last_update = Column(DateTime, default=datetime.now)
 
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String(256), nullable=False)
 
     is_admin = Column(Boolean, default=False)
     is_disabled = Column(Boolean, default=False)
@@ -24,19 +24,19 @@ class UserActivation(Base):
     __tablename__ = "user_activations"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, nullable=False)
+    username = Column(String(128), unique=True, index=True)
+    email = Column(String(128), unique=True, nullable=False)
     created_at = Column(DateTime, default=datetime.now)
 
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String(256), nullable=False)
 
-    secret_code = Column(String, unique=True, nullable=False, index=True)
+    secret_code = Column(String(256), unique=True, nullable=False, index=True)
 
 
 class PasswordChangeRequest(Base):
     __tablename__ = "password_change_requests"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, nullable=True)
+    email = Column(String(128), unique=True, nullable=True)
     created_at = Column(DateTime, default=datetime.now)
-    secret_code = Column(String, unique=True, nullable=False, index=True)
+    secret_code = Column(String(256), unique=True, nullable=False, index=True)
